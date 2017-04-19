a0ce96f2c7a0df7f130e044ae506f346d3edbd12
dylanvans <dylanvansprang@gmail.com>
2017-04-19 13:44:44 +0200

    basic setup project

--- server.js
+++ server.js
@@ -1,41 +1,42 @@
-// Deployed on now webpage:  !!
-
 var express = require('express');
 var app = express();
 var path = require('path');
-
 var bodyParser = require('body-parser');
 var request = require('request');
 
-var oege = 'https://oege.ie.hva.nl/~palr001/icu';
-var host = 'https://oege.ie.hva.nl/~palr001/icu/api.php?t=sdc&d=0197';
+// users [Oliver, Dylan, Nooroel]
+var users = ['0197', '04b7', 'FFA3'];
 
 var colr = 'http://www.colr.org/json/color/random';
-
-var target = 'FFA3';
+var host = users[1];
+var target = users[1];
 
 var randomColors = [];
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.listen(process.env.PORT || 5000, function (){
-    console.log('server is running: on 5000');
-});
+// View engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static(path.join(__dirname, 'static')));
+app.use(express.static('public'));
 
 app.get('/', function(req, res) {
-    // request(host + '&td=' + target + '&c=' + colr + '&m=HOIIIII', function (error, response, body) {
-    request(colr, function (error, response, data) {
-         if(!error && response.statusCode === 200) {
-            color = JSON.parse(data);
-            console.log('output: ' + color.new_color);
-            randomColors.push(color.new_color);
-            console.log(randomColors);
-            // res.render('index', {
-            //     data: data
-            // });
+    request({
+        uri: `http://oege.ie.hva.nl/~palr001/icu/api.php`,
+        qs: {
+            t: 'rdc',
+            t: 'sdc',
+            d: users[1],
+            td: users[1],
+            c: '#6aff00'
         }
-    })
+    });
+
+    res.render('index');
+});
+
+app.listen(process.env.PORT || 5000, function (){
+    console.log('server is running: on 5000');
 });
